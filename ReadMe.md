46d299ecde3a288721fc834d1a8e4bcd9d8e4f99
Dru1DD <mrtproger@gmail.com>
2026-01-04 18:07:11 +0100

    feat: add readme, add prettier

---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+## ZK-Password
+
+ZK-Password is a password manager that uses zero-knowledge proofs to store your passwords.
+
+Tech Stack:
+
+- Electron
+- React
+- Vite
+- TypeScript
+- Zero-Knowledge Proofs
--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,7 +1,7 @@
-import { Container, createRoot } from "react-dom/client";
-import { WelcomeSection } from "./components/welcome-section";
+import { Container, createRoot } from 'react-dom/client';
+import { WelcomeSection } from './components/welcome-section';
 
-const rootContainer = document.getElementById("root");
+const rootContainer = document.getElementById('root');
 const root = createRoot(rootContainer as Container);
 root.render(
   <>
